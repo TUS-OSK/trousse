785e17ed88afff87357cd161701dfcc5bb6c0bcc
momoogles <mo1419091@gmail.com>
2019-09-11 00:11:58 +0900

    コスメ選択画面のコスメのリストを表示

--- src/store/main.js
+++ src/store/main.js
@@ -1,10 +1,47 @@
+import { fetchMain } from '../api'
+
 export default {
   state: {
+    user: {},
+    cosmes: {
+      base: [],
+      cheek: [],
+      lip: []
+    },
+    //コスメを制限して表示、か、全ての一覧を表示のトリガーにしようとしているけど無理そう
+    cosmesState: {
+      base: true,
+      cheek: true,
+      lip: true
+    }
   },
   getters: {
+    user: state => state.user,
+    cosmeTypes: state => Object.keys(state.cosmes),
+    cosmes: state => type => state.cosmes[type],
+    limitedCosmes: state => type => state.cosmes[type].slice(0, 1)
   },
   mutations: {
+    updateMainData(state, payload) {
+      state.user = payload.user
+      state.cosmes = payload.cosmes
+
+      //cosme一つ一つに選択されているかされていないかを判別するプロパティが欲しかっただけなのに...
+      //ここ、もっと他のやり方がありそう、助けてください神様
+      const cosmeTypes = Object.keys(payload.cosmes)
+      cosmeTypes.map(type => {
+        state.cosmes[type].map(cosme => {
+          cosme.state = true
+          return cosme
+        })
+      })
+
+    }
   },
   actions: {
+    async loadMain({ commit }) {
+      const mainData = await fetchMain()
+      commit('updateMainData', mainData)
+    }
   }
 }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,7 +4,20 @@
     <div class="row">
       <Sidebar PageName="メイン"/>
       <main>
-        <h2>Hello World!</h2>
+        <h2>今日のコスメを決めよう！</h2>
+        <div v-for="category in limitedItems" :key="category.label" class="category">
+          <h3 class="category-label">{{ category.label }}</h3>
+          <button class="displayDetail" @click="changeState(category.label)">詳細</button>
+          <ul class="list">
+           <li v-for="item in category.list" :key="item.id" class="item">{{ item.name }}
+              <ul class="cosme-info">
+                <li> {{ item.brand }} </li>
+                <li> {{ item.color }} </li>
+                <li> {{ item.state }} </li>
+              </ul>
+           </li>
+          </ul>
+        </div>
         <router-link class="link" to="/main/result">結果を画像で保存</router-link>
       </main>
     </div>
@@ -16,10 +29,33 @@ import Header from '@/components/Header.vue'
 import Sidebar from '@/components/Sidebar.vue'
 
 export default {
-  name: 'main',
+  name: 'main-page',
   components: {
     Header,
     Sidebar
+  },
+  computed: {
+    limitedItems() {
+      return this.$store.getters['main/cosmeTypes'].map(type => ({
+        label: type,
+        list: this.$store.getters['main/limitedCosmes'](type)
+      })) 
+    },
+    allItems() {
+      return this.$store.getters['main/cosmeTypes'].map(type => ({
+        label: type,
+        list: this.$store.getters['main/cosmes'](type)
+      }))
+    }
+  },
+  methods: {
+    changeState(type) {
+      this.$store.state.main.cosmesState[type] = !this.$store.state.main.cosmesState[type]
+      alert(this.$store.state.main.cosmesState[type])
+    }
+  },
+  created() {
+    this.$store.dispatch('main/loadMain')
   }
 }
 </script>
