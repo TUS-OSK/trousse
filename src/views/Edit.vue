4d62373800eb94989988d9b723c19b5676648bb4
ysdln0616 <1419099@ed.tus.ac.jp>
2019-09-11 12:17:01 +0900

    RR A. コスメのリストを表示

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -11,7 +11,7 @@
             <div class="color">{{ item.color }}</div>
           </li>
         </ul>
-        <div><input type="button" value="コスメを追加" onclick='EditButtonClicked()'></div>
+        <button value="コスメを追加" onclick='editButtonClicked()'>
       </main>
     </div>
   </div>
@@ -34,7 +34,7 @@ export default {
     }
   },
   methods:{
-    EditButtonClicked(){
+    editButtonClicked(){
       this.$store.dispatch('DisplayAddForm')
     }
   },
