304317a47331707c1f876a39b969a79035328eb3
pnly <pnlybubbles@gmail.com>
2019-10-04 05:20:47 +0900

    いろいろやった

--- src/api/index.js
+++ src/api/index.js
@@ -1,58 +1,14 @@
-function mockApi(payload) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(payload)
-    }, 0)
-  })
+import url from 'url'
+
+function endpoint(path) {
+  return url.resolve(process.env.VUE_APP_API_ROOT, path)
 }
 
-const MOCK_FETCH_MAIN = {
-  user: {
-    token: 'mox2847',
-    name: 'mo'
-  },
-  cosmes: {
-    base: [{
-      id: '1',
-      brand: 'CANMAKE',
-      name: 'colorbase-pink',
-      color: 'pink',
-      theme: []
-    },
-    {
-      id: '2',
-      brand: 'CHIFURE',
-      name: '保湿ベース',
-      color: 'beige',
-      theme: []
-    }],
-
-    cheek: [{
-      id: '3',
-      brand: 'CHIFURE',
-      name: 'ツヤツヤベール',
-      color: 'orange',
-      theme: []
-    }],
-
-    lip: [{
-      id: '4',
-      brand: 'Dior',
-      name: 'マキシマイザー',
-      color: 'pink',
-      theme: []
-    },
-    {
-      id: '5',
-      brand: 'MAC',
-      name: 'おとなのりっぷ',
-      color: 'red',
-      theme: []
-    }]
+export async function fetchMain() {
+  const rp = await fetch(endpoint('api/cosmes'))
+  const data = await rp.json()
+  return {
+    user: {},
+    cosmes: data
   }
-}
-
-export function fetchMain() {
-  // テスト用
-  return mockApi(MOCK_FETCH_MAIN)
 }
