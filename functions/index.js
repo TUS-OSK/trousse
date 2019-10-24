8fb47721a5bf76de93ff063bec8ff0f77fae7957
ysdln0616 <1419099@ed.tus.ac.jp>
2019-10-24 12:35:11 +0900

    認証がぁ

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
+console.log("User/yoshidaruna/Downloads", process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
 const functions = require('firebase-functions');
+require("firebase/firestore")
 const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp()
+// admin.initializeApp({
+//   credential: admin.credential.applicationDefault(),
+//   databaseURL: 'https://oskweb-trousse.firebaseio.com'
+// });
 // console.log(functions.config())
 const express = require('express');
 const cors = require('cors')
@@ -11,30 +18,28 @@ const app = express();
 
 app.use(cors())
 
-const fireStore = admin.firestore()
+const db = admin.firestore()
 
-
-const usersRef = fireStore.collection('users');
-usersRef.doc('MOCK_COSMES').set({
-  base: [{
-    id: '1',
-    brand: 'CANMAKE',
-    name: 'colorbase-pink',
-    color: 'pink',
-    theme: []
-  }]
-})
-
+// const usersRef = db.collection('users');
+// usersRef.doc('MOCK_COSMES').set({
+//   base: [{
+//     id: '1',
+//     brand: 'CANMAKE',
+//     name: 'colorbase-pink',
+//     color: 'pink',
+//     theme: []
+//   }]
+// })
 
 
 app.get('/cosmes', async (req, res) => {
-    const usersRef = fireStore.collection('users').doc('MOCK_COSMES')
-    const data = await usersRef.get()
-    console.log(data)
-    console.log(data.data())
-    res.json(data)
+  const usersRef = db.collection('users').doc('MOCK_COSMES')
+  const data = await usersRef.get()
+  res.json(data.data())
 })
 
 module.exports.api = functions
     .region(REGION)
     .https.onRequest(app);
+
+    process.on('unhandledRejection', console.dir);
--- src/components/modules/CosmeFormModal.vue
+++ src/components/modules/CosmeFormModal.vue
@@ -1,36 +1,41 @@
 <template>
-  <div class="cosme-form-modal">
-    <modal :name="`form-modal-${formId}`">
-      <div class="inputs">
-        <div>コスメの名前:
-          <input v-model="cosmeNameText" type="text" name="name" >
+ <div class="cosme-form-modal-component">
+    <modal :name="`form-modal-${formId}`" width="100%" height="70%" :pivotY="1.0">
+      <div class="cosme-form-modal">
+        <div class="ml-inputs">
+          <h3 class="ml-input-title">コスメの名前</h3>
+          <input class="ml-input" v-model="cosmeNameText" type="text" name="name" >
+          <h3 class="ml-input-title">コスメのブランド</h3>
+          <input class="ml-input" v-model="cosmeBrandText">
+          <h3 class="ml-input-title">コスメの色味</h3>
+          <input class="ml-input" v-model="cosmeColorText" type="text" name="color" >
+          <h3 class="ml-input-title">コスメのテーマ</h3>
+          <div class="ml-checkboxes-area">
+            <label class="ml-cb-label" v-for="theme in themes" :key="theme">
+              <input class="ml-cb-input" v-model="cosmeThemeCheckbox" :value="theme" type="checkbox">{{ toJapanese(theme) }}
+            </label>
+          </div>
         </div>
-        <div>コスメのブランド:
-          <input v-model="cosmeBrandText">
+        <div class="buttons" >
+          <div class="ml-update-btns" v-if="formType === 'edit'">
+            <button class="ml-btn ml-delete-btn" v-on:click="showConfirmModal()">コスメを削除</button>
+            <button class="ml-btn ml-change-btn" v-on:click="updateCosmeInfo()">コスメを更新</button>
+          </div>
+          <div class="ml-update-btns" v-else>
+            <button class="ml-btn ml-register-btn" v-on:click="updateCosmeInfo()">コスメを登録</button>
+          </div>
         </div>
-        <div>コスメの色味:
-          <input v-model="cosmeColorText" type="text" name="color" >
+      <modal :name="`delete-modal-${formId}`" width="100%" height="20%" :pivotY="1.0" >
+          <div class="ml-cosme-warning-modal">
+            <h3 class="ml-ml-warning-title">Warning!</h3>
+            <p class="ml-ml-warning-txt">本当に削除しますか？</p>
+            <div class="ml-ml-warning-btns">
+              <button class="ml-btn ml-delete-btn" v-on:click="deleteCosmeInfo()">はい</button>
+              <button class="ml-btn" v-on:click="hideConfirmModal()">いいえ</button>
+            </div>
+          </div>
+        </modal>
         </div>
-        <div>コスメのテーマ:
-          <label v-for="theme in themes" :key="theme">
-            <input v-model="cosmeThemeCheckbox" :value="theme" type="checkbox">{{ toJapanese(theme) }}
-          </label>
-        </div>
-      </div>
-      <div class="buttons" >
-        <div v-if="formType === 'edit'">
-          <button v-on:click="updateCosmeInfo()">コスメ情報を更新</button>
-          <button v-on:click="showConfirmModal()">コスメを削除</button>
-        </div>
-        <div v-else>
-          <button v-on:click="updateCosmeInfo()">コスメを登録</button>
-        </div>
-      </div>
-      <modal :name="`delete-modal-${formId}`">
-        <div>本当に削除しますか？</div>
-        <button v-on:click="deleteCosmeInfo()">はい</button>
-        <button v-on:click="hideConfirmModal()">いいえ</button>
-      </modal>
     </modal>
   </div>
 </template>
@@ -72,23 +77,12 @@ export default {
         cosmeThemeCheckbox: []
       }
     }
-    return{
-      cosmeIdCount: 1,
-      database: null,
-      cosmesRef: null,
-      cosmes: {
-        base: [],
-        cheek: [],
-        lip: []
-      }
-    }
   },
   methods: {
     updateCosmeInfo(){
       const newCosme = {
         type: this.focusingType,
         info: {
-          id: this.focusingCosme.id,
           brand: this.cosmeBrandText,
           name: this.cosmeNameText,
           color: this.cosmeColorText,
@@ -96,12 +90,12 @@ export default {
         }
       }
       if(this.formType === 'edit') {
+        newCosme.info.id = this.focusingCosme.id
         this.$store.dispatch('userData/changeCosmeInfo', newCosme)
         this.$store.dispatch('userData/loadMain')
       } else if(this.formType === 'register') {
         this.$store.dispatch('userData/registerCosmeInfo', newCosme)
         this.$store.dispatch('userData/loadMain')
-
         this.cosmeBrandText = ''
         this.cosmeNameText = ''
         this.cosmeColorText = ''
@@ -142,4 +136,78 @@ export default {
 </script>
 
 <style scoped>
+.cosme-form-modal {
+  padding: 20px;
+}
+.ml-cb-label {
+  display: inline-block;
+  background-color: rgb(243, 234, 183);
+  padding: 8px;
+  margin: 0 12px 12px 0;
+  border-radius: 12px;
+  font-size: 16px;
+  width: auto;
+}
+/* for input */
+.ml-cb-input {
+  margin: 0 4px 0 0;
+}
+.ml-cb-label {
+  border: 2px solid rgb(196, 183, 114);
+}
+.ml-checkboxes-area {
+  margin: 12px;
+}
+.ml-input-title {
+  font-size: 16px;
+}
+.ml-input {
+  border: 2px solid rgb(196, 183, 114);
+  border-radius: 12px;
+  font-size: 16px;
+  background-color: rgb(243, 234, 183);
+  margin: 12px;
+  padding: 8px;
+  width: 85%;
+}
+.ml-input:focus, .ml-cb-input:focus {
+  border: 2px solid rgb(182, 55, 86);
+}
+/* for btns */
+.ml-update-btns {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+.ml-btn {
+  padding: 8px;
+  text-align: center;
+  font-size: 20px;
+  border-radius: 12px;
+  background-color: blanchedalmond;
+  border: 2px solid rgb(196, 183, 114);
+}
+.ml-delete-btn {
+  color: white;
+  background-color: rgb(192, 29, 70);
+  border: 2px solid rgb(192, 29, 70);
+}
+/* for delete modal */
+.ml-ml-warning-title {
+  margin: 12px;
+  text-align: center;
+}
+.ml-ml-warning-txt {
+  text-align: center;
+  margin: 4px;
+}
+.ml-ml-warning-btns {
+  display: flex;
+  justify-content: center;
+}
+.ml-cosme-delete-modal {
+  width: 100px;
+}
+
 </style>
